f2651c6a3c063bb95488360c32732ad4cfbfd8a7
Andrii Bondar <andrey.bondar.ua@gmail.com>
2020-03-15 23:31:16 +0200

    Change color to red

--- CICD/install_docker_presta.sh
+++ CICD/install_docker_presta.sh
@@ -2,19 +2,20 @@
 sudo apt-get update
 #sudo curl -fsSL get.docker.com | sh -
 #delete previous containers
-docker rm -f presta-mysql prestashop
+docker rm -f $(docker ps -q)
+export THEME_NAME=$(jq -r '.THEME_NAME' version.json)
 
 echo "run docker container with prestashop"
-docker-compose up -d -f CICD/docker-compose.yml
-
-
-# copy theme to volume
-sudo cp -r themes/$THEME_NAME /var/lib/docker/volumes/theme_data/_data/var/www/html/themes/$THEME_NAME
-sudo chown -R www-data:www-data /var/lib/docker/volumes/theme_data/_data/var/www/html/themes/$THEME_NAME
+cd CICD
+docker-compose up -d
 
 echo "Wait finishing install script..."
 while [ -z "$(ps -e | grep apache2)" ]; do sleep 10; done
 echo "Install script finished..."
-cd /var/lib/docker/volumes/theme_data/_data/var/www/html
-sudo rm -rf install
-sudo mv admin admin_p
+# copy theme to volume
+cd ~
+sudo cp -r themes/$THEME_NAME /var/lib/docker/volumes/theme_data/_data
+sudo chown -R www-data:www-data /var/lib/docker/volumes/theme_data/_data/$THEME_NAME
+
+
+docker exec prestashop bash -c 'rm -rf install; mv admin admin_p'
